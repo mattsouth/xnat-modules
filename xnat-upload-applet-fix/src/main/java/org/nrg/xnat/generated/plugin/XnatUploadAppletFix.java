c2d2e918c0e84aac392815da1d53b0bd00bf8bb3
Matt South <matthew.south@psych.ox.ac.uk>
2017-04-06 12:14:51 +0100

    after running module to plugin for applet fix and applying ricks 1.7 template;

---
+++ xnat-upload-applet-fix/src/main/java/org/nrg/xnat/generated/plugin/XnatUploadAppletFix.java
@@ -0,0 +1,10 @@
+package org.nrg.xnat.generated.plugin;
+
+import org.nrg.framework.annotations.XnatDataModel;
+import org.nrg.framework.annotations.XnatPlugin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@XnatPlugin(value = "xnat-upload-applet-fix", name = "XNAT Upload Applet Fix", description = "Plugin that provides a short-term fix for the java upload applet page")
+public class XnatUploadAppletFix {
+}
